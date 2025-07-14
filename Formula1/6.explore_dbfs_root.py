55a5409a3f874d1e80936b9666d0f871d395a7cf
pakkiraiah <pakkiraiah>
2025-07-14 03:24:50 +0000

    DBFS ROOT

---
+++ Formula1/6.explore_dbfs_root.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #Explore DBFS Root
+# MAGIC ##1. List All teh folders in DBFS root
+# MAGIC ##2. Interact with DBFS file Browser
+# MAGIC ##3. Upload file to ROOT
+# MAGIC
+
+# COMMAND ----------
+
+formula1_account_key=dbutils.secrets.get(scope="formula1-scope",key="access-key")
+
+# COMMAND ----------
+
+spark.conf.set("fs.azure.account.key.databrickspracticesa.dfs.core.windows.net", formula1_account_key)
+
+# COMMAND ----------
+
+dbutils.fs.ls("abfss://demo@databrickspracticesa.dfs.core.windows.net")
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("abfss://demo@databrickspracticesa.dfs.core.windows.net"));
+
+# COMMAND ----------
+
+display(spark.read.csv("abfss://demo@databrickspracticesa.dfs.core.windows.net/circuits.csv"))
+
+# COMMAND ----------
+
+dbutils.fs.ls("/")
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("/"))
